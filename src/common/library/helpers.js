297a435bf9776d910c67fe6d1afe5da3b22d018b
Ruud <rburger@totalactivemedia.nl>
2013-11-13 17:20:14 +0100

    Semicolon fixes

--- src/common/library/helpers.js
+++ src/common/library/helpers.js
@@ -3,7 +3,7 @@
  */
 
 var $ = function(selector){
-		return document.querySelector(addRandom(selector))
+		return document.querySelector(addRandom(selector));
 	},
 	p = function(){
 		kango.console.log(arguments.length == 1 ? arguments[0] : arguments);
@@ -15,8 +15,9 @@ var sheet,
 
 var addSelector = function(name, properties){
 
-	if(!sheet)
+	if(!sheet){
 		sheet = cssjs.newSheet();
+	}
 
 	// Add unique identifier
 	name = addRandom(name);
@@ -25,7 +26,7 @@ var addSelector = function(name, properties){
 		if(i.indexOf('@') === 0){
 			prefixes.forEach(function(prefix){
 				properties[(prefix ? ('-' + prefix + '-') : '') + i.replace('@', '')] = properties[i];
-			})
+			});
 			delete properties[i];
 		}
 		else {
@@ -37,7 +38,7 @@ var addSelector = function(name, properties){
 				prefixes.forEach(function(prefix){
 					properties[i + ex] = (prefix ? ('-' + prefix + '-') : '') + prop;
 					ex += ' ';
-				})
+				});
 				delete properties[i];
 			}
 		}
@@ -45,7 +46,7 @@ var addSelector = function(name, properties){
 
 	sheet.selector(name, properties);
 
-}
+};
 
 var addRandom = function(selector){
 	// Add unique identifier
@@ -53,54 +54,62 @@ var addRandom = function(selector){
 	selector = selector.replace(/#/gi, '#'+random);
 
 	return selector;
-}
+};
 
 // Create element
 var parseModifiers = function(modifiers){
-    var modifiers = addRandom(modifiers).match(/[#@&:=\.][^#@&:=\.]+/g);
-    
-    if(!modifiers) return {};
-    
+    modifiers = addRandom(modifiers).match(/[#@&:=\.][^#@&:=\.]+/g);
+
+    if(!modifiers){
+		return {};
+    }
+
     var attrs = {},
         i = 0,
         i_max = modifiers.length,
-        prefix, 
+        prefix,
         value;
 
     for (i = 0; i < i_max; ++i){
         prefix = modifiers[i].charAt(0);
         value = modifiers[i].substr(1).replace(/\s+$/, '');
 
-        if (prefix === '#')
+        if (prefix === '#'){
             attrs.id = value;
-        else if (prefix === '.')
+        }
+        else if (prefix === '.'){
             attrs['class'] = attrs['class'] ? attrs['class'] + ' ' + value : value;
+        }
 
     }
-    
+
     return attrs;
-}
-  
+};
+
 var Scaffold = function(){
 	var args = [].splice.call(arguments,0),
 		parent = args[0],
 		parent_el = parent;
-	
+
 	// Create parent if needed
 	if(typeof parent == 'string'){
 		var split = parent.match(/^\s*([a-z0-9]+)\s*((?:[#@&:=\.\[][^#@&:=\.]+\s*)*)$/i);
-		var attrs = parseModifiers(split[2]),
+		var attrs = parseModifiers(split[2]);
 			parent_el = document.createElement(split[1]);
-			
-		for(var i in attrs){
-			parent_el.setAttribute(i, attrs[i]);
+
+		if(attrs && attrs.length > 0){
+			for(var i in attrs){
+				parent_el.setAttribute(i, attrs[i]);
+			}
 		}
 	}
 
-  	args.slice(1).forEach(function(el){
-  		if(el === undefined || el[0] === undefined) return;
-  
-  		if(el instanceof Array){
+	args.slice(1).forEach(function(el){
+		if(el === undefined || el[0] === undefined) {
+			return;
+		}
+
+		if(el instanceof Array){
 			parent_el.appendChild(Scaffold.apply(this, el));
 		}
 		else {
@@ -109,8 +118,8 @@ var Scaffold = function(){
 	});
 
 	return parent_el;
-	
-}
+
+};
 
 function destroy(element){
 	element.parentNode.removeChild(element);
--- src/common/userscript.js
+++ src/common/userscript.js
@@ -8,25 +8,25 @@
 // @require style.js
 // ==/UserScript==
 
-var html_el = document.documentElement,
-	close_alert = null;
+var close_alert = null;
 
-kango.addMessageListener('checkApi', function(event) {
+kango.addMessageListener('checkApi', function() {
 	kango.dispatchMessage('setApi', document.body.getAttribute('data-api'));
 });
 
 // Create popup to add movie
-kango.addMessageListener('showSidebar', function(event){
+kango.addMessageListener('showSidebar', function(){
 
 	if($(addRandom('.popup'))){
-		if(close_alert)
+		if(close_alert){
 			close_alert();
+		}
 		return;
 	}
 
 	var popup = createPopup();
 
-	var c = Scaffold(popup.element,
+	Scaffold(popup.element,
 		['div.loading',
 			['div.message', 'Loading movie']
 		],
@@ -48,149 +48,159 @@ kango.addMessageListener('showSidebar', function(event){
 					['a.add_button ', 'Add']
 				],
 				['div.success.transition']
-			],
+			]
 		]
-	),
-	$c = function(sel){ return popup.element.getElementsByClassName(random+sel)[0]; },
-	$$c = function(sel){ return popup.element.querySelector(addRandom(sel)); };
+	);
 
-	kango.invokeAsync('kango.storage.getItem', 'api', function(api) {
-		var url = window.location.href;
+	var $c = function(sel){ 
+		return popup.element.getElementsByClassName(random+sel)[0];
+	};
 
+	var url = window.location.href;
 
-		queryApi({
-			'url': 'userscript.add_via_url/?url=' + escape(url),
-			'onComplete': function(data){
-				var type = 'movie',
-					media = data[type];
+	queryApi({
+		'url': 'userscript.add_via_url/?url=' + escape(url),
+		'onComplete': function(data){
+			var type = 'movie',
+				media = data[type];
 
-				// Create background
-				var image = media.images.poster_original.length > 0 ? media.images.poster_original[0] : null;
-				if(!image && media.images.backdrop.length > 0)
-					image = media.images.backdrop[0];
-
-				if(image)
-					$c('popup_background').setAttribute('style', 'background-image: url(\''+image+'\')');
-
-				// Title
-				$c('title').insertBefore(document.createTextNode(media.original_title), $c('year'));
-
-				// Year
-				if(media.year)
-					$c('year').appendChild(document.createTextNode(media.year));
-
-
-				// Plot
-				if(media.plot)
-					$c('plot').appendChild(document.createTextNode(media.plot));
-
-
-				// Already exists
-				if(media.in_library){
-					var in_library = [];
-					media.in_library.releases.forEach(function(release){
-						in_library.push(release.quality.label)
-					});
-				}
-
-				if(media.in_wanted && media.in_wanted.profile_id)
-					$c('in_wanted').appendChild(document.createTextNode('Already in wanted list: ' + media.in_wanted.profile.label));
-				else if (in_library)
-					$c('in_wanted').appendChild(document.createTextNode('Already in library: ' + in_library.join(', ')));
-				else
-					addClass($c('in_wanted'), 'hidden');
-
-
-				if(media.titles.length == 1)
-					addClass($c('title_select'), 'hidden');
-				else {
-					var options = '';
-					media.titles.forEach(function(t){
-						options += '<option value="'+t+'">'+t+'</option>';
-					});
-					$c('title_select').innerHTML = options;
-				}
-
-				if(!media.imdb){
-					$c('form').textContent = 'Can\'t find enough data to add this.';
-					return;
-				}
-
-
-				// Get categories
-				queryApi({
-					'url': 'category.list/',
-					'onComplete': function(data){
-						var categories = data.list || [];
-
-						if(categories.length == 0){
-							addClass($c('category_select'), 'hidden');
-							return
-						}
-
-						var options = '';
-						categories.forEach(function(cat){
-							options += '<option value="'+cat.id+'">'+cat.label+'</option>';
-						});
-						$c('category_select').innerHTML += options;
-
-					}
-				});
-
-
-				// Get profiles
-				queryApi({
-					'url': 'profile.list/',
-					'onComplete': function(data){
-						var profiles = data.list || [];
-
-						if(profiles.length == 1){
-							addClass($c('profile_select'), 'hidden');
-							return
-						}
-
-						var options = '';
-						profiles.forEach(function(profile){
-							if(!profile.hide)
-								options += '<option value="'+profile.id+'">'+profile.label+'</option>';
-						})
-						$c('profile_select').innerHTML += options;
-
-					}
-				});
-
-
-				// Add button
-				$c('add_button').addEventListener('click', function(){
-
-					var params = {'identifier': media.imdb};
-					if($c('title_select').value)
-						params['title'] = $c('title_select').value;
-					if($c('category_select').value && $c('category_select').value != 'No category')
-						params['category_id'] = $c('category_select').value;
-					if($c('profile_select').value)
-						params['profile_id'] = $c('profile_select').value;
-
-					queryApi({
-						'url': 'movie.add/',
-						'params': params,
-						'onComplete': function(data){
-
-							$c('success').appendChild(document.createTextNode(data.success ? 'Movie added successfully!' : 'Failed adding movie. Check logs.'));
-
-							setTimeout(function(){
-								addClass($c('form'), 'hide');
-								addClass($c('success'), 'show');
-							}, 100);
-
-						}
-					});
-
-
-				}, false);
-
+			// Create background
+			var image = media.images.poster_original.length > 0 ? media.images.poster_original[0] : null;
+			if(!image && media.images.backdrop.length > 0){
+				image = media.images.backdrop[0];
 			}
-		})
+
+			if(image){
+				$c('popup_background').setAttribute('style', 'background-image: url(\''+image+'\')');
+			}
+
+			// Title
+			$c('title').insertBefore(document.createTextNode(media.original_title), $c('year'));
+
+			// Year
+			if(media.year){
+				$c('year').appendChild(document.createTextNode(media.year));
+			}
+
+
+			// Plot
+			if(media.plot){
+				$c('plot').appendChild(document.createTextNode(media.plot));
+			}
+
+
+			// Already exists
+			var in_library = [];
+			if(media.in_library){
+				media.in_library.releases.forEach(function(release){
+					in_library.push(release.quality.label);
+				});
+			}
+
+			if(media.in_wanted && media.in_wanted.profile_id){
+				$c('in_wanted').appendChild(document.createTextNode('Already in wanted list: ' + media.in_wanted.profile.label));
+			}
+			else if (in_library){
+				$c('in_wanted').appendChild(document.createTextNode('Already in library: ' + in_library.join(', ')));
+			}
+			else {
+				addClass($c('in_wanted'), 'hidden');
+			}
+
+
+			if(media.titles.length === 1){
+				addClass($c('title_select'), 'hidden');
+			}
+			else {
+				var options = '';
+				media.titles.forEach(function(t){
+					options += '<option value="'+t+'">'+t+'</option>';
+				});
+				$c('title_select').innerHTML = options;
+			}
+
+			if(!media.imdb){
+				$c('form').textContent = 'Can\'t find enough data to add this.';
+				return;
+			}
+
+
+			// Get categories
+			queryApi({
+				'url': 'category.list/',
+				'onComplete': function(data){
+					var categories = data.list || [];
+
+					if(categories.length === 0){
+						addClass($c('category_select'), 'hidden');
+						return;
+					}
+
+					var options = '';
+					categories.forEach(function(cat){
+						options += '<option value="'+cat.id+'">'+cat.label+'</option>';
+					});
+					$c('category_select').innerHTML += options;
+
+				}
+			});
+
+
+			// Get profiles
+			queryApi({
+				'url': 'profile.list/',
+				'onComplete': function(data){
+					var profiles = data.list || [];
+
+					if(profiles.length === 1){
+						addClass($c('profile_select'), 'hidden');
+						return;
+					}
+
+					var options = '';
+					profiles.forEach(function(profile){
+						if(!profile.hide){
+							options += '<option value="'+profile.id+'">'+profile.label+'</option>';
+						}
+					});
+					$c('profile_select').innerHTML += options;
+
+				}
+			});
+
+
+			// Add button
+			$c('add_button').addEventListener('click', function(){
+
+				var params = {'identifier': media.imdb};
+					params.title = $c('title_select').value;
+
+				if($c('category_select').value && $c('category_select').value != 'No category'){
+					params.category_id = $c('category_select').value;
+				}
+				if($c('profile_select').value){
+					params.profile_id = $c('profile_select').value;
+				}
+
+				queryApi({
+					'url': 'movie.add/',
+					'params': params,
+					'onComplete': function(data){
+
+						$c('success').appendChild(document.createTextNode(data.success ? 'Movie added successfully!' : 'Failed adding movie. Check logs.'));
+
+						setTimeout(function(){
+							addClass($c('form'), 'hide');
+							addClass($c('success'), 'show');
+						}, 100);
+
+					}
+				});
+
+
+			}, false);
+
+		}
 
 	});
 
@@ -210,9 +220,10 @@ var queryApi = function(options){
 		};
 
 		kango.xhr.send(details, function(data) {
-			if(data.status == 200 && data.response != null) {
-				if(options.onComplete)
+			if(data.status == 200 && data.response !== null) {
+				if(options.onComplete){
 					options.onComplete(data.response);
+				}
 			}
 			else {
 				alert('Something went wrong: ' + data.status);
@@ -220,13 +231,13 @@ var queryApi = function(options){
 		});
 	});
 
-}
+};
 
 
 /*
  * Create popup on the edge of the screen
  */
-var createPopup = function(content){
+var createPopup = function(){
 	var body = document.body,
 		html = body.parentNode;
 
@@ -276,7 +287,8 @@ var createPopup = function(content){
 
 			close_alert = null;
 		}
-	}
+
+	};
 
 	// Close alerts
 	document.addEventListener('keyup', close_alert, false);
@@ -286,6 +298,6 @@ var createPopup = function(content){
 	return {
 		'element': popup,
 		'close': close_alert
-	}
+	};
 
-}
+};
